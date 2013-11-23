bf4149a2fab03ef1a25317f3f8f850cbc96ab174
Alcides Fonseca <me@alcidesfonseca.com>
2013-11-23 18:37:17 +0000

    Bumped to Django 1.6

--- urls.py
+++ urls.py
@@ -1,5 +1,5 @@
 from django.conf import settings
-from django.conf.urls.defaults import *
+from django.conf.urls import patterns, url, include
 from django.contrib import admin
 
 admin.autodiscover()
